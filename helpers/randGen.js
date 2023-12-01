a7b609c1ea59dcdb3131d4e8d9dd81ca5a692807
zakywtf <novazaky1@gmail.com>
2023-12-01 18:41:21 +0700

    Feat: Random String Length

--- helpers/randGen.js
+++ helpers/randGen.js
@@ -5,8 +5,11 @@
         if (pin == true) {
             var charset = "0123456789"
         } else {
-            // var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-            var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+            if (length > 10) {
+                var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            } else {
+                var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+            }
         }
         retVal = "";
         for (var i = 0, n = charset.length; i < length; ++i) {
